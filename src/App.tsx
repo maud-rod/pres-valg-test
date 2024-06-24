46f2430270c0dec5710d7cd6239b09b23518539d
Maud Rødsmoen <maudonna@Maud-sin-MacBook-Pro.local>
2024-06-24 13:07:20 +0200

    test av endring2

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
 
   return (
     <>
-    <h1>Presidentvalget i USA</h1>
+    <h1>Presidentvalget i USA, her Pål</h1>
     {/*<button onClick={handlePost}>Post</button>*/}
     <TextField value={biden_votes} onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
     set_biden_votes(Number(event.target.value));
