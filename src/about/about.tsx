cef8f9a0aec77ea28015c05ad8ff363c7fe55d51
Maud Rødsmoen <maudonna@Maud-sin-MacBook-Pro.local>
2024-06-25 14:45:32 +0200

    hei

---
+++ src/about/about.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+/* i stedet for root, mer beskrivende navn */
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <h1>Hi Even!!!!!</h1>
+  </React.StrictMode>,
+)
